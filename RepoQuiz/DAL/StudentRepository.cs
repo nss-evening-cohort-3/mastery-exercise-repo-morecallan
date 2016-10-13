91ac56baf523e64fb464bbfb36d7c631ddaa8b2c
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-13 17:50:52 -0500

    repo with predicted methods

---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RepoQuiz.Models;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepository
+    {
+        public StudentContext Context { get; set; }
+
+        public StudentRepository
+()
+        {
+            Context = new StudentRepository();
+        }
+
+        public StudentRepository(StudentContext _context)
+        {
+            Context = _context;
+        }
+
+        public List<Students> GetStudents()
+        {
+            return Context.Authors.ToList();
+        }
+
+        public Student CheckIfStudentIsInDatabaseByName(string name)
+        {
+            Student found_student = Context.Students.FirstOrDefault(a => a.Name.toLower() == name.ToLower());
+            return found_student;
+        }
+
+        public void AddStudent(Student student)
+        {
+
+            Context.Students.Add(author);
+            Context.SaveChanges();
+        }
+
+        public void AddStudent(string first_name, string last_name, string major)
+        {
+            Student student = new Student { FirstName = first_name, LastName = last_name, Major = major };
+            Context.Students.Add(student);
+            Context.SaveChanges();
+        }
+       
+    }
+}
