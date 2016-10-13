3f1c214f8727ea4592fead4401903187008d3ba0
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-12 22:57:41 -0500

    update to student model for KEY

--- RepoQuiz/Models/Student.cs
+++ RepoQuiz/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,8 @@ namespace RepoQuiz.Models
 {
     public class Student
     {
-        public int StudentID { get; set; }
+        [Key]
+        public int StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Major { get; set; }
