69ff3980d5a3f48653b3f1dfaf98a58332d348b2
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-13 18:47:23 -0500

    init test mocking

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Moq;
+using System.Data.Entity;
+using System.Linq;
+using RepoQuiz.Models;
+using RepoQuiz.DAL;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
     {
+        Mock<StudentContext> mock_context { get; set; }
+        Mock<DbSet<Student>> mock_student_table { get; set; }
+        List<Student> student_list { get; set; }
+        StudentRepository repo { get; set; }
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = student_list.AsQueryable();
+
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            mock_context.Setup(c => c.Students).Returns(mock_student_table.Object);
+
+            mock_student_table.Setup(t => t.Add(It.IsAny<Student>())).Callback((Student s) => student_list.Add(s));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<StudentContext>();
+            mock_student_table = new Mock<DbSet<Student>>();
+            student_list = new List<Student>();
+            repo = new StudentRepository(mock_context.Object);
+
+            ConnectMocksToDatastore();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null; 
+        }
     }
 }
