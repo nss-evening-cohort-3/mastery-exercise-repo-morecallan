e2dc3c93302558a72870d902820b0e86d34c96e8
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-14 11:32:34 -0500

    random student generator tested to extent I can think of using random

--- RepoQuiz.Tests/DAL/NameGeneratorTests.cs
+++ RepoQuiz.Tests/DAL/NameGeneratorTests.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoQuiz.DAL;
+using RepoQuiz.Models;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class NameGeneratorTests
     {
+        [TestMethod]
+        public void NameGeneratorCanBeInstantiated()
+        {
+            //Arrange
+            //Act
+            NameGenerator name_generator = new NameGenerator();
+            //Assert
+            Assert.IsNotNull(name_generator);
+        }
 
+        [TestMethod]
+        public void NameGeneratorStudentGeneratorWillReturnAStudent()
+        {
+            //Arrange
+            NameGenerator name_generator = new NameGenerator();
+            //Act
+            Student random_student = name_generator.CreateNewRandomStudent();
+            //Assert
+            Assert.IsInstanceOfType(random_student, typeof(Student));
+        }
     }
 }
